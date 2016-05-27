aa5630b576b48345f32c83274d9d271cb5b1dbd9
Sergey Rubtsov <rsv.astana@gmail.com>
2016-05-27 10:57:48 +0600

    Возможность отмены

--- FileSystemAttachmentsRelocation/MainWindowViewModel.cs
+++ FileSystemAttachmentsRelocation/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Threading;
@@ -20,6 +21,7 @@ namespace FileSystemAttachmentsRelocation
             InfoMessages = new ObservableCollection<string>();
             CurrentAttachmentsFolder = @"C:\Uploads\TTK";
             _dispatcher = Dispatcher.CurrentDispatcher;
+            _cancellationToken = new CancellationTokenSource();
         }
 
         public ObservableCollection<Attachment> AttachmentsToRelocation { get; set; }
@@ -28,6 +30,7 @@ namespace FileSystemAttachmentsRelocation
 
         private ICommand _getNotInCurrentDir;
         private Dispatcher _dispatcher;
+        private readonly CancellationTokenSource _cancellationToken;
 
         public ICommand GetNotInCurrentDir
         {
@@ -59,17 +62,22 @@ namespace FileSystemAttachmentsRelocation
                         return;
                     }
                     var attachments = AttachmentsToRelocation.ToList();
-                    Task.Run(() => ProcessAttachments(attachments));
+                    Task.Run(() => ProcessAttachments(attachments, _cancellationToken.Token), _cancellationToken.Token);
                 });
             }
         }
 
-        private void ProcessAttachments(IList<Attachment> attachmentsToRelocation)
+        private void ProcessAttachments(IList<Attachment> attachmentsToRelocation, CancellationToken token)
         {
             Log("Start");
             foreach (var attachment in attachmentsToRelocation)
             {
                 ProcessAttachment(attachment);
+                if (token.IsCancellationRequested)
+                {
+                    Log("Process cancelled!");
+                    break;
+                }
             }
             Log("Done!");
         }
@@ -105,6 +113,18 @@ namespace FileSystemAttachmentsRelocation
         }
 
         public ObservableCollection<string> InfoMessages { get; set; }
+
+        public ICommand StopProcess
+        {
+            get
+            {
+                return new CommandHandler(() =>
+                {
+                    _cancellationToken.Cancel();
+                    AttachmentsToRelocation.Clear();
+                });
+            }
+        }
     }
 
     public class CommandHandler : ICommand
